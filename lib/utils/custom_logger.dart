c90de79427dde2dd0bcf3fa45e6be0344925498f
jorge.anttoonio <jorge.anttoonio@gmail.com>
2024-12-19 09:09:00 +0000

    solucion web v1

--- lib/utils/custom_logger.dart
+++ lib/utils/custom_logger.dart
@@ -1,38 +1,46 @@
-import 'dart:io'; // Importa la librería para manejar archivos
-import 'package:logger/logger.dart'; // Importa el paquete de logger para manejar logs
-import 'package:path_provider/path_provider.dart'; // Importa path_provider para obtener el directorio de la aplicación
+import 'dart:io' as io;
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:logger/logger.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CustomLogger {
-  // Instancia privada de CustomLogger
   static final CustomLogger _instance = CustomLogger._internal();
-
-  // Instancia de Logger del paquete logger
   final Logger logger;
 
-  // Constructor privado
   CustomLogger._internal() : logger = Logger();
 
-  // Método estático para acceder a la instancia
   factory CustomLogger() {
-    return _instance; // Retorna la instancia única
+    return _instance;
   }
 
-  // Método para registrar un mensaje en un archivo
   Future<void> logToFile(String message) async {
-    final directory = await getApplicationDocumentsDirectory(); // Obtiene el directorio de documentos
-    final file = File('${directory.path}/app_logs.txt'); // Define el archivo de log
-    await file.writeAsString('$message\n', mode: FileMode.append); // Escribe el mensaje en el archivo
+    if (!kIsWeb) {  // Solo ejecutar en plataformas no web
+      try {
+        final directory = await getApplicationDocumentsDirectory();
+        final file = io.File('${directory.path}/app_logs.txt');
+        await file.writeAsString('$message\n', mode: io.FileMode.append);
+      } catch (e) {
+        print('Error writing to log file: $e');
+      }
+    }
   }
 
-  // Método para registrar información
   void logInfo(String message) {
-    logger.i(message); // Registra el mensaje usando Logger
-    logToFile(message); // También registra el mensaje en el archivo
+    if (kIsWeb) {
+      print('INFO: $message');  // Versión simplificada para web
+    } else {
+      logger.i(message);
+      logToFile(message);
+    }
   }
 
-  // Método para registrar errores
   void logError(String message, [dynamic error]) {
-    logger.e(message, error); // Registra el mensaje de error usando Logger
-    logToFile(message); // También registra el mensaje en el archivo
+    if (kIsWeb) {
+      print('ERROR: $message');  // Versión simplificada para web
+      if (error != null) print(error);
+    } else {
+      logger.e(message, error);
+      logToFile('ERROR: $message${error != null ? '\n$error' : ''}');
+    }
   }
 }
